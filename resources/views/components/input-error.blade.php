d969d800a6c9baacbf8ab853aa619e464f5d98ea
jcsegurat2309 <jcsegurat@gmail.com>
2023-10-05 22:00:53 -0600

    Vista Login

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -3,7 +3,7 @@
 @if ($messages)
     <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
         @foreach ((array) $messages as $message)
-            <li>{{ $message }}</li>
+            <li class="text-red-800 p-4 bg-red-100 border-l-4 border-red-700">{{ $message }}</li>
         @endforeach
     </ul>
 @endif
--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -73,23 +73,20 @@ new #[Layout('layouts.guest')] class extends Component
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form wire:submit="login">
+        <!--Message Error-->
+        <div>
+            <x-input-error :messages="$errors->all()" class="my-3" />
+        </div>
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" autofocus autocomplete="username" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input wire:model="password" id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <x-text-input wire:model="password" id="password" class="block mt-1 w-full" type="password" name="password" autocomplete="current-password" />           
         </div>
 
         <!-- Remember Me -->
@@ -100,16 +97,18 @@ new #[Layout('layouts.guest')] class extends Component
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}" wire:navigate>
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+        <div class="flex items-center justify-between mt-4">
+            
+            <x-normal-link :href="route('register')">
+                Crear cuenta
+            </x-normal-link>
 
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+            <x-normal-link :href="route('password.request')">
+                Olvidaste tu contraseña?
+            </x-normal-link>
         </div>
+        <x-primary-button class="w-full mt-5 justify-center">
+            {{ __('Iniciar Sesión') }}
+        </x-primary-button>
     </form>
 </div>
